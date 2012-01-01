b1ff4ce27bbcaa2d7b5e49414ba984cd88eb213b
Matt Rogers <mattr@kde.org>
2012-01-01 13:10:48 -0600

    Move the contents of the SimpleNote lib here.
    This is done in the interest of having a single self contained app. Once
    the library and the app mature a bit more, then we can separate it back
    out again and version it properly.

--- SimpleNotes/src/com/github/simplenotes/Note.java
+++ SimpleNotes/src/com/github/simplenotes/Note.java
@@ -1,133 +1,138 @@
 /*
-  Copyright (C) 2011  Mattias Svala
-
-  This file is part of SimpleNotes.
-
-  SimpleNotes is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  package com.github.simplenotes;
+ * This POJO is based on the JSON document in the API2.
+ * {
+ key         :  (string, note identifier, created by server),
+ deleted     :  (bool, whether or not note is in trash),
+ modifyDate  :  (last modified date, in seconds since epoch),
+ createDate  :  (note created date, in seconds since epoch),
+ syncNum     :  (integer, number set by server, track note changes),
+ version     :  (integer, number set by server, track note content changes),
+ minVersion  :  (integer, number set by server, minimum version available for note),
+ shareKey    :  (string, shared note identifier),
+ publishKey  :  (string, published note identifier),
+ systemTags  :  [ (Array of strings, some set by server) ],
+ tags        :  [ (Array of strings) ],
+ content     :  (string, data content)
+ }
 */
-
 package com.github.simplenotes;
 
+import java.util.Collection;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+
+import com.google.gson.Gson;
 
 public class Note {
     private Long id;
     private String key;
     private boolean deleted;
-    private Date modifyDate;
-    private Date createDate;
+    private int modifyDate;
+    private int createDate;
     private int syncNum;
-    private int version;
     private int minVersion;
     private String shareKey;
     private String publishKey;
-    private List<String> systemTags;
-    private List<String> tags;
+    private Collection<String> systemTags;
+    private Collection<String> tags;
     private String content;
-
+    
     public Note() {
-        deleted = false;
-        systemTags = new ArrayList<String>();
-        tags = new ArrayList<String>();
-    }
+        this.deleted = false;
+        this.modifyDate = (int)(System.currentTimeMillis() / 1000L);
+        this.createDate = (int)(System.currentTimeMillis() / 1000L);
+        this.tags = null;
+        this.systemTags = null;
 
+    }
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-    public String getKey() {
-        return key;
-    }
     public void setKey(String key) {
         this.key = key;
     }
-    public boolean isDeleted() {
-        return deleted;
+    public String getKey() {
+        return key;
     }
     public void setDeleted(boolean deleted) {
         this.deleted = deleted;
     }
-    public Date getModifyDate() {
-        return modifyDate;
+    public boolean isDeleted() {
+        return deleted;
     }
-    public void setModifyDate(Date modifyDate) {
+    public void setModifyDate(int modifyDate) {
         this.modifyDate = modifyDate;
     }
-    public Date getCreateDate() {
-        return createDate;
+    public int getModifyDate() {
+        return modifyDate;
     }
-    public void setCreateDate(Date createDate) {
+    public void setCreateDate(int createDate) {
         this.createDate = createDate;
     }
-    public int getSyncNum() {
-        return syncNum;
+    public int getCreateDate() {
+        return createDate;
     }
     public void setSyncNum(int syncNum) {
         this.syncNum = syncNum;
     }
-    public int getVersion() {
-        return version;
-    }
-    public void setVersion(int version) {
-        this.version = version;
-    }
-    public int getMinVersion() {
-        return minVersion;
+    public int getSyncNum() {
+        return syncNum;
     }
     public void setMinVersion(int minVersion) {
         this.minVersion = minVersion;
     }
-    public String getShareKey() {
-        return shareKey;
+    public int getMinVersion() {
+        return minVersion;
     }
     public void setShareKey(String shareKey) {
         this.shareKey = shareKey;
     }
-    public String getPublishKey() {
-        return publishKey;
+    public String getShareKey() {
+        return shareKey;
     }
     public void setPublishKey(String publishKey) {
         this.publishKey = publishKey;
     }
-    public List<String> getSystemTags() {
-        return systemTags;
+    public String getPublishKey() {
+        return publishKey;
     }
-    public void setSystemTags(List<String> systemTags) {
+    public void setSystemTags(Collection<String> systemTags) {
         this.systemTags = systemTags;
     }
-    public List<String> getTags() {
-        return tags;
+    public Collection<String> getSystemTags() {
+        if (this.systemTags == null) {
+            this.systemTags = new ArrayList<String>();
+        }
+
+        return this.systemTags;
     }
-    public void setTags(List<String> tags) {
+    public void setTags(Collection<String> tags) {
         this.tags = tags;
     }
-    public String getContent() {
-        return content;
+    public Collection<String> getTags() {
+        if (this.tags == null) {
+            this.tags = new ArrayList<String>();
+        }
+
+        return this.tags;
     }
     public void setContent(String content) {
         this.content = content;
     }
-
-    public boolean isPinned() {
-        return systemTags.contains("pinned");
+    public String getContent() {
+        return content;
     }
-    public boolean isUnread() {
-        return systemTags.contains("unread");
+
+    public static Note fromJSON(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, Note.class);
+    }
+
+    public String toJSON()
+    {
+        Gson gson = new Gson();
+        return gson.toJson(this);
     }
 }
---
+++ SimpleNotes/src/com/github/simplenotes/SimpleNoteAPI.java
@@ -0,0 +1,46 @@
+package com.github.simplenotes;
+
+import java.io.IOException;
+
+public interface SimpleNoteAPI {
+
+    /**
+     * Login to the simple notes service
+     * @throws IOException
+     */
+    public abstract void login() throws IOException;
+
+    /**
+     * Get a note based on it's key as returned by the SimpleNotes service
+     * @param key the key to use when finding the note
+     * @return the note found by key
+     * @throws IOException
+     */
+    public abstract Note get(String key) throws IOException;
+
+    /**
+     * Sync a note to the SimpleNotes service
+     * @param note the note to sync
+     * @return The note that was synced, updated with the information added
+     * by the SimpleNotes service
+     * @throws IOException
+     */
+    public abstract Note add(Note note) throws IOException;
+
+    /**
+     * Set the username used for all transactions with the SimpleNote service
+     */
+    public abstract void setUserName(String userName);
+    
+    /**
+     * Get the username used for all transactions with the SimpleNote service
+     * @return the username used with the SimpleNote service
+     */
+    public abstract String getUserName();
+    
+    /**
+     * Set the password to use for the account
+     */
+    public abstract void setPassword(String password);
+
+}
